1ef18dcc064f2286a637b0c9172ce402d4b3b03d
Bob-huang-gdut <3570411064@qq.com>
2017-06-13 00:48:30 +0800

    添加新文件

---
+++ css/new.js
@@ -0,0 +1,69 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'row-bottom-padded-md': {
+    'paddingBottom': [{ 'unit': 'em', 'value': 2 }]
+  },
+  'fh5co-section-gray': {
+    'background': 'rgb(248, 248, 248)'
+  },
+  'fh5co-testimonial': {
+    'position': 'relative',
+    'background': '#fff',
+    'padding': [{ 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }],
+    'borderRadius': '5px',
+    'marginBottom': [{ 'unit': 'px', 'value': 30 }]
+  },
+  'fh5co-team': {
+    'position': 'relative',
+    'background': '#fff',
+    'padding': [{ 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 30 }],
+    'borderRadius': '5px',
+    'marginBottom': [{ 'unit': 'px', 'value': 30 }]
+  },
+  '#main-logo': {
+    'marginLeft': [{ 'unit': 'px', 'value': -150 }],
+    '<w767': {
+      'marginLeft': [{ 'unit': 'px', 'value': 0 }],
+      'fontSize': [{ 'unit': 'px', 'value': 26 }]
+    }
+  },
+  'service': {
+    'minHeight': [{ 'unit': 'px', 'value': 288 }]
+  },
+  '#footer p': {
+    'color': 'white',
+    'fontSize': [{ 'unit': 'px', 'value': 16 }]
+  },
+  'shareBottom': {
+    'marginBottom': [{ 'unit': 'px', 'value': 24 }]
+  },
+  '#line::after': {
+    'display': 'none'
+  },
+  // @media screen and (min-width: 319px){
+    .fh5co-cover, .fh5co-hero {
+        height: 476px;
+    }
+}
+@media screen and (min-width: 359px){
+    .fh5co-cover, .fh5co-hero {
+        height: 543px;
+    }
+}
+@media screen and (min-width: 374px){
+    .fh5co-cover, .fh5co-hero {
+        height: 572px;
+    }
+}
+@media screen and (min-width: 411px){
+    .fh5co-cover ,.fh5co-hero {
+        height: 635px;
+    }
+}
+@media screen and (min-width: 413px){
+    .fh5co-cover ,.fh5co-hero {
+        height: 645px;
+    }
+}
+});
---
+++ css/new2.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'bshare-custom bshare-more': {
+    'paddingLeft': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'string', 'value': '!important' }],
+    'color': '#9bcf7f !important',
+    'display': 'inline-block'
+  },
+  'bshare-customicon-medium-plus bshare-weixin': {
+    'background': 'url(../images/weixin.png) no-repeat!important',
+    'display': 'inline-block'
+  },
+  'bsTop': {
+    'width': [{ 'unit': 'px', 'value': 208 }]
+  },
+  'bsFrameDiv>div': {
+    'height': [{ 'unit': 'px', 'value': 209 }],
+    'width': [{ 'unit': 'px', 'value': 220 }]
+  },
+  'bsFrameDiv img': {
+    'width': [{ 'unit': 'px', 'value': 168 }],
+    'height': [{ 'unit': 'px', 'value': 169 }]
+  },
+  'shareBottom': {
+    'marginBottom': [{ 'unit': 'px', 'value': 24 }]
+  },
+  '#line::after': {
+    'display': 'none'
+  }
+});
